198abee281d3ae534441f26ca882609e627f7a01
Aizar Rahima <aizarrahima@gmail.com>
2022-03-18 22:57:02 +0700

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import Header from "./Header";
 import Note from "./Note";
+import notes from "../Notes";
 import Footer from "./Footer";
 
 function App() {
-    return <div>
-        <Header />
-        <Note />
-        <Footer />
-    </div>
+    return (
+        <div>
+            <Header />
+            {notes.map(item => (
+                <Note
+                    key={item.key}
+                    title={item.title}
+                    content={item.content}
+                />
+            ))}
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
+
+// item = forEach
